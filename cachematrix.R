b5c2cb842b0b31fafc9b475dc593021298219a32
Angelo Varlotta <5478922+varlottaang@users.noreply.github.com>
2021-01-27 14:03:25 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+# The following function calculates the inverse of the special "matrix" created with the above function. 
+# However, it first checks to see if the inverse has already been calculated. 
+# If so, it gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the data and sets the value of the mean in the cache
+# via the setinverse function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+# Example
+# > prova <-matrix(c(6,3,5,1,6,8,6,9,2), 3,3)
+# > inverso <-makeCacheMatrix(prova)
+# > cacheSolve(inverso)
+#             [,1]        [,2]        [,3]
+#[1,]  0.16806723 -0.12885154  0.07563025
+#[2,] -0.10924370  0.05042017  0.10084034
+#[3,]  0.01680672  0.12044818 -0.09243697
+# > cacheSolve(inverso)
+# getting cached data
+
